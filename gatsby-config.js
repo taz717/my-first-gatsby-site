c8915fb9cb632c9d052c57afb5734a5b91cc70c7
taz717 <moatazkhallaf@outlook.com>
2023-05-01 14:29:14 -0600

    part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,16 @@
-/**
- * @type {import('gatsby').GatsbyConfig}
- */
 module.exports = {
   siteMetadata: {
-    title: `My Gatsby Site`,
-    siteUrl: `https://www.yourdomain.tld`,
+    title: "My First Gatsby Site",
   },
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
   ],
-}
+};
